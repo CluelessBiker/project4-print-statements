8002aab4277a4764fc37b18617b0be7908e850e9
Lauren-Nicole <popich.red@gmail.com>
2022-07-01 07:20:27 +0000

    Update to admin.py to access blog comments.

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, BlogComments
 from django_summernote.admin import SummernoteModelAdmin
 
 
+# Class used from "I think therefore I blog" walkthrough.
 @admin.register(BlogPost)
 class PostAdmin(SummernoteModelAdmin):
     """
     Making the summernote text editor field
     availble in the BlogPost admin panel
     """
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
+
+
+# Class used from "I think therefore I blog" walkthrough.
+@admin.register(BlogComments)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Providing admin access to moderate
+    submitted comments
+    """
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
