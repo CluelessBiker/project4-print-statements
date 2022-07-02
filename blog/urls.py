9aeb2c28df1633c9647c28e8bf63055a51308a8e
Lauren-Nicole <popich.red@gmail.com>
2022-07-02 13:04:57 +0000

    Resolve bug. Fix import statment in views. Add comma to urls path.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePage, name='home'),
     path('blog/', views.BlogPage.as_view(), name='blog'),
-    path('<slug:slug>/', views.BlogPostPage.as_view(), name='blog_post')
+    path('<slug:slug>/', views.BlogPostPage.as_view(), name='blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 """
 Functions for the view code,
 """
-from django.shortcuts import render, get_object_404
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import BlogPost
 
@@ -31,7 +31,7 @@ class BlogPostPage(View):
     """
     def get(self, request, slug, *args, **kwargs):
         queryset = BlogPost.objects.filter(status=1)
-        post = get_object_404(queryset, slug=slug)
+        post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('-created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
