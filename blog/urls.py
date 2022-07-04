5d20c00035c7552b4bb82acf1963217fd5e97688
Lauren-Nicole <popich.red@gmail.com>
2022-07-04 10:54:06 +0000

    Add url path for like/unlike feature.

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('blog/', views.BlogPage.as_view(), name='blog'),
     path('<slug:slug>/', views.BlogPostPage.as_view(), name='blog_post'),
+    path('like/<slug:slug>', views.LikeBlogPost.as_view(), name='like_post'),
 ]
