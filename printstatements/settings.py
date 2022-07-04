41eecec18f2cebc9b1a1dae450f15ae14f4182ed
Lauren-Nicole <popich.red@gmail.com>
2022-07-04 13:20:09 +0000

    Media query added for mobile devices.

--- printstatements/settings.py
+++ printstatements/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['print-statements.herokuapp.com', 'localhost']
 
 
