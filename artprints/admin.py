47b104111b9c0e1321013771caf23138867c36d2
Lauren-Nicole <popich.red@gmail.com>
2022-07-07 12:21:17 +0000

    Update admin panel with search feature. Pre-populate slug field.

--- artprints/admin.py
+++ artprints/admin.py
@@ -6,4 +6,9 @@ from django.contrib import admin
 from .models import ArtPrint
 
 
-admin.site.register(ArtPrint)
+@admin.register(ArtPrint)
+class PrintAdmin(admin.ModelAdmin):
+    prepoulated_fields = {'slug': ('print_name',)}
+    list_display = ('artist', 'status', 'created_on')
+    list_filter = ('artist', 'created_on')
+    search_fields = ['print_name', 'artist']
