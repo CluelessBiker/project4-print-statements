d3d18f1908690dfd6f1da42c6775dbcd95011e4e
Lauren-Nicole <popich.red@gmail.com>
2022-07-07 11:41:58 +0000

    Create new app: Artprints

---
+++ artprints/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArtprintsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'artprints'
--- printstatements/settings.py
+++ printstatements/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'crispy_forms',
     'blog',
+    'artprints',
 ]
 
 SITE_ID = 1
