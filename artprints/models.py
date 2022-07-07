8e1ac09f5aa9e4e655c31375dbaf3efcf14c16e9
Lauren-Nicole <popich.red@gmail.com>
2022-07-07 11:45:19 +0000

    Add model for art prints.

--- artprints/models.py
+++ artprints/models.py
@@ -1,3 +1,50 @@
+"""
+Database models for the Art Prints app
+"""
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class ArtPrint(models.Model):
+    """
+    Database model for printed artworks
+    """
+    print_name = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    slug = models.SlugField(max_length=50, unique=True, null=False, blank=False)
+    artist = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='art_prints')
+    description = models.TextField()
+    artwork_image = CloudinaryField('image', null=False, blank=False)
+    print_height = models.IntegerField(null=False, blank=False)
+    print_width = models.IntegerField(null=False, blank=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+    print_run = models.IntegerField(null=False, blank=False)
+    remaining_copies = models.IntegerField(null=False, blank=False)
+    price = models.IntegerField(null=False, blank=False)
+    likes = models.ManyToManyField(User, related_name='print_likes', blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        """
+        To diplay the most recently
+        uploaded artwork first
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """
+        To display the print name
+        instead of the model ID.
+        """
+        return self.print_name
+
+    def number_of_likes(self):
+        """
+        To return total number of print likes
+        """
+        return self.likes.count()
