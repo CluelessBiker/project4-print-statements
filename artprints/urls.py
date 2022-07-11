c6cb56f147f0abbb2bcf45cd6310e43b3fd0480b
Lauren-Nicole <popich.red@gmail.com>
2022-07-11 07:46:58 +0000

    Add button to navigate to submission page.

--- artprints/urls.py
+++ artprints/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('prints/', views.PrintsPage.as_view(), name='prints'),
     path('<slug:slug>/', views.PrintDetails.as_view(), name='print_detail'),
     path('like/<slug:slug>', views.LikeArtPrint.as_view(), name='like_print'),
+    path('submit-print/', views.submit_art_print, name='submit-print'),
 ]
