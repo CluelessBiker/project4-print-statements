98ca03a34fe7b6de9f857fa05af7ba837ecfec5e
Lauren-Nicole <popich.red@gmail.com>
2022-07-12 10:16:13 +0000

    Add URL path to delete print.

--- artprints/urls.py
+++ artprints/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('prints/', views.PrintsPage.as_view(), name='prints'),
     path('submitprint/', views.submit_art_print, name='submit_print'),
     path('edit/<slug:slug>', views.edit_art_print, name='edit_print'),
+    path('delete/<slug:slug>', views.delete_art_print, name='delete_print'),
     path('<slug:slug>/', views.PrintDetails.as_view(), name='print_detail'),
     path('like/<slug:slug>', views.LikeArtPrint.as_view(), name='like_print'),
 ]
