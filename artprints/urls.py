704d789b7383ee99c3097a210d794046b585c3a9
Lauren-Nicole <popich.red@gmail.com>
2022-07-12 09:59:11 +0000

    Create URL path for edit page.

--- artprints/urls.py
+++ artprints/urls.py
@@ -8,6 +8,7 @@ from . import views
 urlpatterns = [
     path('prints/', views.PrintsPage.as_view(), name='prints'),
     path('submitprint/', views.submit_art_print, name='submit_print'),
+    path('edit/<slug:slug>', views.edit_art_print, name='edit_print'),
     path('<slug:slug>/', views.PrintDetails.as_view(), name='print_detail'),
     path('like/<slug:slug>', views.LikeArtPrint.as_view(), name='like_print'),
 ]
